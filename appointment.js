1e84bdaca2dfe4c791625580dd487ca77c41d508
navadeep1012 <navadeep1012@gmail.com>
2023-03-14 22:55:43 +0530

    storing the users in right format dom task

--- appointment.js
+++ appointment.js
@@ -6,24 +6,20 @@ function test(){
     var utime = document.getElementById('time').value;
 
     // // Storing the retrived data as strings
-    // let uname_serialized = JSON.stringify(uname);
-    // let uemail_serialized = JSON.stringify(uemail);
-    // let upwd_serialized = JSON.stringify(upwd);
-    // let utime_serialized = JSON.stringify(utime);
+    let uname_serialized = JSON.stringify(uname);
+    let uemail_serialized = JSON.stringify(uemail);
+    let upwd_serialized = JSON.stringify(upwd);
+    let utime_serialized = JSON.stringify(utime);
 
 
 
 
     // //storing data in the localstorage
-    // var username = localStorage.setItem("uname", uname_serialized);
-    // var useremail = localStorage.setItem("uemail", uemail_serialized);
-    // var userpwd = localStorage.setItem("upwd", upwd_serialized);
-    // var usertime = localStorage.setItem("utime", utime_serialized);
+    var username = localStorage.setItem("uname", uname_serialized);
+    var useremail = localStorage.setItem("uemail", uemail_serialized);
+    var userpwd = localStorage.setItem("upwd", upwd_serialized);
+    var usertime = localStorage.setItem("utime", utime_serialized);
 
-    var username = localStorage.setItem("uname", uname);
-    var useremail = localStorage.setItem("uemail", uemail);
-    var userpwd = localStorage.setItem("upwd", upwd);
-    var usertime = localStorage.setItem("utime", utime);
 
      
    
