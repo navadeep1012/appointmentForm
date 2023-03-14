f890260d0f0b0dcfa365395e0bd28f248eca7ae4
navadeep1012 <navadeep1012@gmail.com>
2023-03-14 22:51:28 +0530

    store the users in right format task

--- appointment.js
+++ appointment.js
@@ -4,10 +4,27 @@ function test(){
     var uemail = document.getElementById('email').value;
     var upwd = document.getElementById('password').value;
     var utime = document.getElementById('time').value;
-    //storing data in the localstorage
+
+    // // Storing the retrived data as strings
+    // let uname_serialized = JSON.stringify(uname);
+    // let uemail_serialized = JSON.stringify(uemail);
+    // let upwd_serialized = JSON.stringify(upwd);
+    // let utime_serialized = JSON.stringify(utime);
+
+
+
+
+    // //storing data in the localstorage
+    // var username = localStorage.setItem("uname", uname_serialized);
+    // var useremail = localStorage.setItem("uemail", uemail_serialized);
+    // var userpwd = localStorage.setItem("upwd", upwd_serialized);
+    // var usertime = localStorage.setItem("utime", utime_serialized);
+
     var username = localStorage.setItem("uname", uname);
     var useremail = localStorage.setItem("uemail", uemail);
     var userpwd = localStorage.setItem("upwd", upwd);
     var usertime = localStorage.setItem("utime", utime);
+
+     
    
 }
