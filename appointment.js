3de681d969645832cc15947f6a14feb1411ee85e
navadeep1012 <navadeep1012@gmail.com>
2023-03-15 19:20:12 +0530

    showing the localstorage data on ui

--- appointment.js
+++ appointment.js
@@ -1,23 +1,62 @@
-function test(){
-    //Retreiving data
-    var uname = document.getElementById("name").value;
-    var uemail = document.getElementById('email').value;
-    var upwd = document.getElementById('password').value;
-    var utime = document.getElementById('time').value;
+function saveToLocalStorage(event){
+            event.preventDefault();
+            const name = event.target.name.value;
+            const email = event.target.email.value;
+            const phone = event.target.phone.value;
+
+            const obj = {
+                name,
+                email,
+                phone
+            }
+            localStorage.setItem(obj.email, JSON.stringify(obj))
+            const parentElem = document.getElementById('list');
+            const childElem = document.createElement('li')
+            childElem.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phone
+            parentElem.appendChild(childElem);
+            //showUserOnScreen()
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // // Storing the retrived data as strings
-    let uname_serialized = JSON.stringify(uname);
-    let uemail_serialized = JSON.stringify(uemail);
-    let upwd_serialized = JSON.stringify(upwd);
-    let utime_serialized = JSON.stringify(utime);
+    // let uname_serialized = JSON.stringify(uname);
+    // let uemail_serialized = JSON.stringify(uemail);
+    // let uphone_serialized = JSON.stringify(uphone);
+    // let utime_serialized = JSON.stringify(utime);
+
 
 
 
 
     // //storing data in the localstorage
-    var username = localStorage.setItem("uname", uname_serialized);
-    var useremail = localStorage.setItem("uemail", uemail_serialized);
-    var userpwd = localStorage.setItem("upwd", upwd_serialized);
-    var usertime = localStorage.setItem("utime", utime_serialized);
+    // var username = localStorage.setItem("uname", uname_serialized);
+    // var useremail = localStorage.setItem("uemail", uemail_serialized);
+    // var userpwd = localStorage.setItem("uphone", uphone_serialized);
+    // var usertime = localStorage.setItem("utime", utime_serialized);
+
+
+     
    
-}
+
